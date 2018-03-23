adb59f9e219d1e032e40feadd6e999855247f58c
Felix Gladisch <gladisch@sipgate.de>
2018-03-23 16:59:09 +0100

    tests: corrupted chain

--- src/blockchain.model.test.ts
+++ src/blockchain.model.test.ts
@@ -31,10 +31,21 @@ describe("Blockchain", () => {
 	it("should overwrite the existing chain", () => {
 		const blockchain: Blockchain = new Blockchain();
 		blockchain.generateNextBlock(TEST_DATA);
-		const copy: Blockchain = new Blockchain([...blockchain.getChain()]);
+		const copy: Blockchain = new Blockchain(blockchain.getChain().slice());
 		copy.generateNextBlock(TEST_DATA);
 		assert.lengthOf(blockchain.getChain(), 2);
 		blockchain.replaceChain(copy.getChain());
 		assert.lengthOf(blockchain.getChain(), 3);
+	});
+
+	it("should not overwrite the existing chain with a corrupted chain", () => {
+		const blockchain: Blockchain = new Blockchain();
+		blockchain.generateNextBlock(TEST_DATA);
+		const copy: Blockchain = new Blockchain(blockchain.getChain().slice());
+		copy.generateNextBlock(TEST_DATA);
+		const corruptedChain: Block[] = copy.getChain().slice();
+		corruptedChain.splice(1, 1);
+		blockchain.replaceChain(corruptedChain);
+		assert.notEqual(blockchain.getChain(), corruptedChain);
 	});
 });
